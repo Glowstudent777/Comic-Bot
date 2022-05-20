c78414ca584a3bbd8d4913366db53d11ea4e7717
Glowstudent <cameronmenken7@gmail.com>
2022-05-19 23:22:39 -0500

    autopost

--- events/autopost.js
+++ events/autopost.js
@@ -1,15 +1,12 @@
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
-const dotenv = require('dotenv');
-require('dotenv').config();
 const { MessageEmbed } = require('discord.js');
-const { getImage } = require("gocomics-api");
-// const moment = require('moment');
 var moment = require("moment");
 var momentDurationFormatSetup = require("moment-duration-format");
 const date = require('date-and-time');
 const autopost = require('../config/autopost.json');
 const colors = require('../config/config.json');
+const { parse } = require("node-html-parser");
+const rp = require("request-promise-native");
+const r = require("request");
 
 module.exports = {
     name: 'ready',
@@ -17,66 +14,28 @@ module.exports = {
     async execute(client) {
 
         console.log("Starting autoposting...");
-
-        function checkDate(x) {
-            if (date.isValid(x, 'YYYY-M-D')) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
         async function getComic() {
-            let cdate;
-            // cdate = moment().subtract(Math.floor(Math.random() * (moment().diff(moment('1978-06-19', 'YYYY-MM-DD')) / 86400000)), 'days').format('YYYY-MM-DD');
+            var archive = 'https://gocomics.com/garfield/';
+            var today = new Date();
+            var todayYear = today.getFullYear();
+            var todayMonth = today.getMonth();
+            var todayDay = today.getDate();
+            var url = archive + todayYear + '/' + todayMonth + '/' + todayDay;
 
-            cdate = moment().format('YYYY-MM-DD');
-            cdate = moment(cdate).add(1, 'months').format('YYYY-MM-DD');
+            const parsedPage = parse(await rp(url)
+                .catch(err => {
+                    console.log("Request failed\n", err);
+                }));
 
-            cdate = date.transform(cdate, 'YYYY-MM-DD', 'YYYY-M-D');
+            const imageURL = parsedPage.querySelector(".item-comic-image img").rawAttrs.split(/ src=/)[1].replace(/"/g, "");
 
-            // Month replacement
-            if (cdate.split('-')[1] === '0') cdate = cdate.split('-')[0] + '-2-' + cdate.split('-')[2];
-            if (cdate.split('-')[1] === '1') cdate = cdate.split('-')[0] + '-2-' + cdate.split('-')[2];
-
-            console.log(`Is valid: ${date.isValid(cdate, 'YYYY-M-D')}`);
-
-            // Check if date is valid before trying to get comic x3
-            for (let i = 0; i < 3; i++) {
-                if (checkDate(cdate) === false) {
-                    console.log("Error, comic not found. Retrying...");
-                    return getComic();
-                }
-            }
-
-            if (!date.isValid(cdate, 'YYYY-M-D')) {
-                console.log("Error, comic not found. Retrying...");
-                return getComic();
-            }
-
-            const comic = await getImage({
-                comicName: "garfield",
-                comicNumber: Math.floor(Math.random() * 100),
-                comicFormat: "png",
-                // URLOnly: true,
-                date: [cdate],
-            }).catch(err => {
-                console.log("Error, comic not found. Retrying...");
-                return getComic();
-            });
-
-            let footerDate;
-            footerDate = moment(cdate, 'YYYY-M-D').subtract(1, 'months').format('YYYY-MM-DD');
-            footerDate = cdate.split('-').join('/');
-
-            const comicEmbed = new MessageEmbed()
+            const embed = new MessageEmbed()
                 .setColor(colors.colors.main)
-                .setTitle(`Today's Garfield Comic!`)
-                .setImage(comic.uri.href)
-                .setFooter({ text: `Comic from: ${footerDate}` })
+                .setTitle('Garfield')
+                .setURL(url)
+                .setImage(imageURL)
+                .setFooter({ text: 'Powered by GoComics.com' })
                 .setTimestamp();
-            // return await message.channel.send({ embeds: [comicEmbed] });
 
             for (let i = 0; i < autopost.channels.length; i++) {
                 client.guilds.cache.forEach(guild => {
@@ -92,8 +51,8 @@ module.exports = {
 
                             if (!channel.permissionsFor(client.user).has('SEND_MESSAGES')) return console.log(`I don't have permission to send messages in ${channel.name}`);
 
-                            if (ping === "") return channel.send({ embeds: [comicEmbed] });
-                            else return channel.send({ content: `<@&${ping}>`, embeds: [comicEmbed] });
+                            if (ping === "") return channel.send({ embeds: [embed] });
+                            else return channel.send({ content: `<@&${ping}>`, embeds: [embed] });
                         }
                     });
                 });
